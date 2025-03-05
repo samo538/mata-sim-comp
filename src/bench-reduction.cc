9abb575308ba8fb9e65ca9e2022293918190a800
Samuel Luptak <xluptas00@stud.fit.vutbr.cz>
2025-03-05 12:09:59 +0100

    Src modified

--- src/bench-reduction.cc
+++ src/bench-reduction.cc
@@ -3,13 +3,13 @@
  *  - If you want to skip mintermization, set the variable `MINTERMIZE_AUTOMATA` below to `false`
  */
 
-#include "mata/nfa/algorithms.hh"
 #include "utils/utils.hh"
 #include "mata/nfa/nfa.hh"
 
 #include <iostream>
 #include <iomanip>
 #include <mata/nfa/types.hh>
+#include <ostream>
 #include <string>
 
 using namespace mata::nfa;
@@ -32,42 +32,47 @@ int main(int argc, char *argv[])
     if (load_automaton(filename, aut, alphabet, MINTERMIZE_AUTOMATA) != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
-
+    
     // Setting precision of the times to fixed points and 4 decimal places
     std::cout << std::fixed << std::setprecision(4);
 
+    // Evaluate time
     if (algorithm == "simulation"){
-        TIME_BEGIN(relation);
+        TIME_BEGIN(reduction);
     
-        result = mata::nfa::reduce(aut, 0, ParameterMap{{"algorithm", algorithm}});
+        result = mata::nfa::reduce(aut, 0, ParameterMap{{"algorithm", algorithm}, {"direction", "forward"}});
 
-        TIME_END(relation);
-
+        TIME_END(reduction);
     }
     else if (algorithm == "residual"){
-        TIME_BEGIN(relation);
+        TIME_BEGIN(reduction);
 
         result = mata::nfa::reduce(aut, 0, ParameterMap{{"algorithm", algorithm}, {"type", "with"}, {"direction", "forward"}});
 
-        TIME_END(relation);
+        TIME_END(reduction);
     }
     else if (algorithm == "brzozowski"){
-        TIME_BEGIN(relation);
+        TIME_BEGIN(reduction);
 
         result = mata::nfa::minimize(aut, ParameterMap{{"algorithm", algorithm}});
 
-        TIME_END(relation);
+        TIME_END(reduction);
     }
-    else if (algorithm == "hopkroft"){
-        TIME_BEGIN(relation);
+    else if (algorithm == "hopcroft"){
+        TIME_BEGIN(reduction);
 
         result = mata::nfa::minimize(aut, ParameterMap{{"algorithm", algorithm}});
 
-        TIME_END(relation);
+        TIME_END(reduction);
     }
     else{
         return EXIT_FAILURE;
     }
+
+    // Evaluate the number of states and transitions
+    std::cout << "states: " << result.num_of_states() << std::endl;
+    std::cout << "transitions: " << result.delta.num_of_transitions() << std::endl;
+
 
     return EXIT_SUCCESS;
 }
